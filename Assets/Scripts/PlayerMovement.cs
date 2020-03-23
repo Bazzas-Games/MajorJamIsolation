a38b645dfb1c9cc85ca97068037942a06ddfca0a
Mungo_ <bailey.gibbons2018@gmail.com>
2020-03-23 21:11:31 +1100

    Player faces mouse position

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        faceMouse();
+        
+    
+    }
+
+
+    void faceMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 aimDir = new Vector2(
+            mousePos.x - transform.position.x,
+            mousePos.y - transform.position.y
+        );
+        transform.up = aimDir;
+
+
+    }
+}

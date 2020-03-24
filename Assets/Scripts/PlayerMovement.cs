96adb37803cffd4a06d0c08a7a8826e10bf30eb4
Mungo_ <bailey.gibbons2018@gmail.com>
2020-03-24 22:47:02 +1100

    Player can now pick up objects

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public GameObject heldObject;
     public float grabDistance = 1f;
-    private Rigidbody2D rb;
+    public LayerMask grabbableObjects;
 
+    private Rigidbody2D rb;
+    private Vector2 aimDir = new Vector2();
+    [SerializeField]
+    private Transform holdPoint;
 
     void Start()
     {
@@ -17,8 +21,20 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         FaceMouse();
-        
-    
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(heldObject == null)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, aimDir, grabDistance, grabbableObjects);
+                Debug.DrawRay(transform.position, aimDir.normalized);
+                if(hit.collider != null && hit.collider.GetComponent<Throwable>() != null)
+                {
+                    Grab(hit.collider.gameObject);
+                    Debug.Log("Hit! " + hit.collider.gameObject.name);
+                }
+            }
+        }
     }
 
 
@@ -30,17 +46,18 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        Vector2 aimDir = new Vector2(
+        aimDir = new Vector2(
             mousePos.x - transform.position.x,
             mousePos.y - transform.position.y
         );
         transform.up = aimDir;
-
-
     }
 
-    void Grab()
+    void Grab(GameObject obj)
     {
-
+        heldObject = obj;
+        heldObject.transform.parent = holdPoint;
+        heldObject.transform.localPosition = Vector3.zero;
+        heldObject.transform.localRotation = Quaternion.identity;
     }
 }

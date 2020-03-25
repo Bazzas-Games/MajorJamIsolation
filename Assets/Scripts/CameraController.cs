4cdcd2545c30be100b137d04dee45270c9fe1b70
Mungo_ <bailey.gibbons2018@gmail.com>
2020-03-25 22:37:52 +1100

    Started adding throwing mechanic Bug: drag direction is not correct

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 0, -10); // make sure camera can see everything
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public GameObject heldObject;
-    public float grabDistance = 1f;
+    public Throwable heldObject;
+    public Transform holdPoint;
     public LayerMask grabbableObjects;
+    public float grabDistance = 1f;
+    public float maxForce = 10f;
+    public float maxVel = 5f;
+    public float pullbackDistance = 100000f;
+    public float thrustForce = 1f;
 
+    private Animator animator;
     private Rigidbody2D rb;
+    private Vector2 mousePos2D;
+    private Vector2 mouseClickPos;
     private Vector2 aimDir = new Vector2();
-    [SerializeField]
-    private Transform holdPoint;
+    private Vector2 dragDirection = Vector2.zero;
+    private float dragDistRaw = 0; // distance from mouse to click point, can be negative
+    private float dragDist = 0; // clamped distance from mouse to click point, 0 - dragInputScale
+    private float dragMult = 0; // 0 - 1, how close is mouse to dragInputScale?
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        FaceMouse();
+        // Microthrusters
+        Vector2 thrusters = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.AddForce(thrusters.normalized * thrustForce);
 
-        if (Input.GetMouseButtonDown(0))
+
+        mousePos2D = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))  // grab object if left clicked
         {
             if(heldObject == null)
             {
@@ -30,39 +46,64 @@ public class PlayerMovement : MonoBehaviour
                 Debug.DrawRay(transform.position, aimDir.normalized);
                 if(hit.collider != null && hit.collider.GetComponent<Throwable>() != null)
                 {
-                    Grab(hit.collider.gameObject);
+                    Grab(hit.collider.gameObject.GetComponent<Throwable>());
                     Debug.Log("Hit! " + hit.collider.gameObject.name);
                 }
             }
         }
+        if(Input.GetMouseButtonDown(1)) // start charging
+        {
+            mouseClickPos = mousePos2D;
+        }
+        if (Input.GetMouseButton(1)) // charge throw
+        {
+            if (heldObject != null)
+            {
+                dragDirection = Camera.main.WorldToScreenPoint(transform.position);
+                dragDirection = dragDirection - mouseClickPos;
+                dragDistRaw = Vector2.Dot(mouseClickPos - dragDirection, mouseClickPos - mousePos2D);
+                dragDist = Mathf.Clamp(dragDistRaw, 0, pullbackDistance);
+                dragMult = dragDist / pullbackDistance;
+
+                Debug.DrawLine(mouseClickPos, mouseClickPos - dragDirection, Color.blue);
+                animator.SetBool("isDragging", true);
+                animator.SetFloat("Blend", dragMult);
+            }
+            else FaceMouse();
+        }
+        else {          // unless charging a throw, face the mouse.
+            FaceMouse();
+            animator.SetBool("isDragging", false);
+        }
     }
-
-
-    
-    
 
 
     void FaceMouse()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        aimDir = new Vector2(
-            mousePos.x - transform.position.x,
-            mousePos.y - transform.position.y
-        );
+        aimDir = Camera.main.ScreenToWorldPoint(mousePos2D) - transform.position;
         transform.up = aimDir;
     }
 
-    void Grab(GameObject obj)
+    void Grab(Throwable obj)
     {
         heldObject = obj;
         Collider2D c = heldObject.GetComponent<Collider2D>();
         c.enabled = false;
-        Rigidbody2D heldObjectRB = heldObject.GetComponent<Rigidbody2D>();
+        Rigidbody2D heldObjectRB = heldObject.gameObject.GetComponent<Rigidbody2D>();
         heldObjectRB.isKinematic = true;
        
         heldObject.transform.parent = holdPoint;
         heldObject.transform.localPosition = Vector3.zero;
         heldObject.transform.localRotation = Quaternion.identity;
+    }
+
+
+    void Throw()
+    {
+
+        if (!heldObject.isAnchored)
+        {
+
+        }
     }
 }

bebdb512689a29c840c4bfe3d56da5dc3306780b
Kownur <62537455+Kownur@users.noreply.github.com>
2020-03-28 14:39:15 +1100

    Basic Movement Input Built
    Movement mechanics for rooms with Gravity.
    Next step is working on grab and pull mechanics, I think I can largely use what we have previously built for zeroG movement

---
+++ Assets/Scripts/GravityEnabledMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityEnabledMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+
+    // Update is called once per frame
+    // Input
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Movement
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+	}
+
+}

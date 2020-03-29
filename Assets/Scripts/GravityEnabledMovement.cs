4fbef863ec7d892e535c9075eaddd24906de39b3
Kownur <62537455+Kownur@users.noreply.github.com>
2020-03-29 13:42:21 +1100

    Update GravityEnabledMovement.cs
    Added Mouse tracking for Facing Character

--- Assets/Scripts/GravityEnabledMovement.cs
+++ Assets/Scripts/GravityEnabledMovement.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class GravityEnabledMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
 
     Vector2 movement;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
 
     // Update is called once per frame
     // Input
     void Update()
     {
+        faceMouse();
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
+
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
+
+	}
 
     // Movement
     void FixedUpdate()

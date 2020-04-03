566c9d580f244dd6c0bd507a78caacc681b39f9a
Kownur <62537455+Kownur@users.noreply.github.com>
2020-04-04 00:43:39 +1100

    cannot end game

---
+++ Assets/Scripts/Testing/EndGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "EndPlatform") 
+        {
+            SceneManager.LoadScene("Menu");
+        }
+	}
+}

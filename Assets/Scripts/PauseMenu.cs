bee996a93f7210e7473d11de008df01a63a53cc9
Kownur <62537455+Kownur@users.noreply.github.com>
2020-04-07 20:42:48 +1000

    Added Pause State and Menu
    Current Menu to Game Scene load is bugged and not reloading correctly.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When "Escape" is pressed, Resume game if Pause. Pause game if not.
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+               Resume(); 
+			} else
+            {
+               Pause();
+			}
+        } 
+    }
+
+    // Bring Down Pause Mene.
+    // Unfreeze Time in Game.
+    // Change GameIsPaused to False.
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+	}
+
+    // Bring up Pause Menu.
+    // Freeze Time in Game.
+    // Change GameIsPaused to True.
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+	}
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+	}
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+	}
+}
